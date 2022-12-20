3fe08a5c0d5cfe337e2a1ca6d27e449ed4aca21d
yogajulianp <yogajprasutiyo@gmail.com>
2022-12-20 07:20:59 +0700

    depend on

--- src/main/java/com/rapidtech/demospringboot/config/DependOnConfiguration.java
+++ src/main/java/com/rapidtech/demospringboot/config/DependOnConfiguration.java
@@ -2,11 +2,17 @@ package com.rapidtech.demospringboot.config;
 
 import com.rapidtech.demospringboot.model.Bar;
 import com.rapidtech.demospringboot.model.Foo;
-import org.springframework.context.annotation.Bean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.*;
 
-import static com.sun.java.util.jar.pack.Utils.log;
+//import static com.sun.java.util.jar.pack.Utils.log;
 
+@Slf4j
+@Configuration
 public class DependOnConfiguration {
+
+    @Lazy
+    @DependsOn(value = {"foo"})
     @Bean
     public Bar bar(){
         log.debug("Creating new Bar");
@@ -14,8 +20,10 @@ public class DependOnConfiguration {
     }
 
     @Bean
+    @Scope("prototype")
     public Foo foo(){
         log.debug("Creating new Foo");
+        return new Foo();
     }
 
 }
--- src/test/java/com/rapidtech/demospringboot/config/DependOnConfigurationTest.java
+++ src/test/java/com/rapidtech/demospringboot/config/DependOnConfigurationTest.java
@@ -1,6 +1,11 @@
 package com.rapidtech.demospringboot.config;
 
+import com.rapidtech.demospringboot.model.Bar;
+import com.rapidtech.demospringboot.model.Foo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +13,18 @@ class DependOnConfigurationTest {
 
     @Test
     void dependOnTest() {
-        App
+        ApplicationContext context = new AnnotationConfigApplicationContext(DependOnConfiguration.class);
+        Foo foo1 = context.getBean(Foo.class);
+        Foo foo2 = context.getBean(Foo.class);
+        Foo foo3 = context.getBean(Foo.class);
+
+        Assertions.assertNotSame(foo1, foo2);
+        Assertions.assertNotSame(foo1, foo3);
+
+
+//        Bar bar1 = context.getBean(Bar.class);
+//        Bar bar2 = context.getBean(Bar.class);
+//        Assertions.assertSame(bar1, bar2);
+
     }
 }
